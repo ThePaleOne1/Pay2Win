ecb741201764d13808224ba571161309ceabdfca
Lachlan <mindcraftlachlan@outlook.com>
2020-03-10 01:19:34 +1100

    very simple working prototype
    both worlds have at least something, however it's missing the whole premise of pay to win right now

---
+++ Assets/PCinteract.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PCinteract : MonoBehaviour
+{
+    [SerializeField]
+    GameObject prompt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        prompt.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.tag == "Player") 
+        {
+            prompt.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                SceneManager.LoadScene("GameScene");
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            prompt.SetActive(true);
+        }
+    }
+}
