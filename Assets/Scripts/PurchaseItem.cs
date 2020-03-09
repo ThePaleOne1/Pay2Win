de386694d5d482d90c1d49452d789813a16dd4d0
Lachlan <mindcraftlachlan@outlook.com>
2020-03-10 01:47:20 +1100

    rudimentary pay to win system
    a button to buy cubes and a text object to show how much money the player has

---
+++ Assets/Scripts/PurchaseItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurchaseItem : MonoBehaviour
+{
+    [SerializeField]
+    GameObject ItemPrefab;
+    [SerializeField]
+    float cost = 1;
+    [SerializeField]
+    GameObject player;
+    MoneyCount mc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mc = player.GetComponent<MoneyCount>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void purchaseItem()
+    {
+        if (mc.moneyCount >= cost) 
+        {
+            mc.moneyCount -= cost;
+            Instantiate(ItemPrefab, player.transform.position + new Vector3(Random.Range(-1, 1), Random.Range(0, 1), 0), player.transform.rotation);
+        }
+    }
+}
